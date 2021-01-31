36e2abf1ced19e71907cb4efad78a7208b5b9108
Sathish Reddy <sathishreddy.dev3@gmail.com>
2021-01-31 12:38:59 -0500

    Add project files.

---
+++ Unit_Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Assignment1_Sathish
+{
+    class Unit_Test
+    {
+        static void Main11(string[] args)
+        {
+            string[,] paths = new string[,] { { "London", "New York" }, { "New York", "Tampa" },
+                                        { "Delhi", "London" } };
+            string destination = DestCity(paths);
+            Console.WriteLine("Q6");
+            Console.WriteLine("Destination city is " + destination);
+
+        }
+        private static string DestCity(string[,] paths)
+        {
+            try
+            {
+                         return "";
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+
+    }
+}
